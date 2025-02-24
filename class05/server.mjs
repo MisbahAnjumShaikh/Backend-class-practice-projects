ce78ea7530287ba3daa3e5076e77212f5f2fda8a
MisbahAnjumShaikh <misbahanjumshaikh05pk@gmail.com>
2025-02-24 21:05:26 +0500

    Test GET endpoints with Postman

--- class05/server.mjs
+++ class05/server.mjs
@@ -1,13 +1,17 @@
 import express from "express"
 const app = express()
-const port = 3000
-// const port = process.env.PORT || 3000
+const port =  process.env.PORT || 5000 
 
+// 192.168.228.126:3000 check get requests
 app.get('/', (req, res) => {
   res.send('Hello get requests!')
 })
 
-// 192.168.228.126:3000 check get requests
+
+app.get("/allusers", (req, res)=>{
+    res.send("get all users!")
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
